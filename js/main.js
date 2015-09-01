837bb8c23cb7774d88b8926f521ac3bc91c123d0
Allison Keane Barr <allisonkeane@gmail.com>
2015-09-01 15:44:34 -0400

    refactored, now fully functioning

--- js/main.js
+++ js/main.js
@@ -1,187 +1,133 @@
 //data for tic tac toe board
 
-var squares = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+var squaresData = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-
-
-//STATE
-
-//state capturing whether it is the player's turn
-
-var playersTurn = true
-
-//state capturing whether game is in progress or done
-
-var gameActive = true
-
-//state capturing whether the player has won
-
-var playerWins = false
+var squaresElems = [].slice.call(document.querySelectorAll("td"))
 
 
 
 //GAMEPLAY
 
-//if it is user's turn, display message telling them to play
+//Select an item from the squaresData array with a value of 0 and return its index
 
-var promptPlayer = function() {
-  if (playersTurn) {
-    alert("Your turn! Click on an empty square to mark it with an X.")
-  }
+var aiSelect = function() {
+  var availSquareIndexes = squaresData.reduce(function(newArray, currentItem, index) {
+    if (currentItem === 0) {
+      return newArray.concat(index)
+    } else {
+      return newArray
+    }
+  }, [])
+  return availSquareIndexes[Math.floor(Math.random() * availSquareIndexes.length)]
 }
 
-//capture user click on a td. In squares array, check if corresponding item is blank.  If so, update to 1. If not, do not change.
 
-var playerSelection =
+//Called on selection of a square. In squaresData array, check if corresponding item is blank.  If so, update to 1 if player, 2 if AI. If not, do not change.
 
-//if corresponding item in squares array has a 1, add "<i class="fa fa-times fa-4x"></i>" to the td
-
-var showXMark =
-
-//select an item from the squares array with a value of 0 and change it to 2
-
-var aiSelection =
-
-//if corresponding item in squares array has a 2, add "<i class="fa fa-circle-o fa-4x"></i>" to the td
-
-var showOMark =
+var assignSquare = function(index, whoseTurn) {
+  var data = squaresData[index]
+  if (data === 0) {
+    if (whoseTurn === "player") {
+      squaresData[index] = 1
+      squaresElems[index].innerHTML = "X"
+    } else {
+      squaresData[index] = 2
+      squaresElems[index].innerHTML = "O"
+    }
+    return true
+  } else {
+    return false
+  }
+}
 
 
 
 //WIN COMBOS
 
-//function for checking if all values in an array are equal
+//indexes for win combos
 
-Array.prototype.allValuesSame = function() {
-    for(var i = 1; i < this.length; i++)
-    {
-        if(this[i] !== this[0])
-            return false;
-    }
-    return true;
-}
-
-//if squares[x, y, z] are all equal to 1, return "player"; if all equal to 2, return "AI"; else return false.
-
-var squareChecker = function(x, y, z) {
-  if ([squares[x], squares[y], squares[z]].allValuesSame()) {
-    if (squares[x] === 0) {
-      return false
-    } else if (squares[x] === 1) {
-      return "player"
-    } else {
-      return "AI"
-    }
-  } else {
-    return false
-  }
-}
-
-//apply squareChecker to all the win combos
-
-var checkers = [
-  var threeAcrossTop = function() {
-    squareChecker(0, 1, 2)
-  },
-  var threeAcrossCenter = function() {
-    squareChecker(3, 4, 5)
-  },
-  var threeAcrossBottom = function() {
-    squareChecker(6, 7, 8)
-  },
-  var threeDownLeft = function() {
-    squareChecker(0, 3, 6)
-  },
-  var threeDownMiddle = function() {
-    squareChecker(1, 4, 7)
-  },
-  var threeDownRight = function() {
-    squareChecker(2, 5, 8)
-  },
-  var threeDiagonalLeft = function() {
-    squareChecker(0, 4, 8)
-  },
-  var threeDiagonalRight = function() {
-    squareChecker(2, 4, 6)
-  },
+var winCombos = [
+  [ 0, 1, 2 ],
+  [ 3, 4, 5 ],
+  [ 6, 7, 8 ],
+  [ 0, 3, 6 ],
+  [ 1, 4, 7 ],
+  [ 2, 5, 8 ],
+  [ 0, 4, 8 ],
+  [ 2, 4, 6 ],
 ]
 
+//if data points referenced by combo INDEXES are all equal to 1 or all equal to 2, return true; else return false.
 
-//END OF TURN - the below needs to be done at the end of each turn
-
-//check if any of the win combos return "player" or "AI"; if they return "player", set playerWins to true;
-//if they return either "player" or "AI", set gameActive state to false
-
-var checkWinCombos = function() {
-  if (checkers.some(function(winner) {return winner === "player"})) {
-      playerWins = true
-      gameActive = false
-      return true
-  } else {
-      if (checkers.some(function(winner) {return winner === "AI"})) {
-        gameActive = false
-        return true
-      } else {
-        return false
-      }
-  }
+var checkWinCombo = function(combo) {
+  return (
+    combo.every(function(item) { return squaresData[item] === 1 }) ||
+    combo.every(function(item) { return squaresData[item] === 2 })
+  )
 }
 
-//check if any of the remaining squares have a value of 0, and if not set gameActive state to false
+//return true if any win combos have been met
+
+var checkWinCombos = function() {
+  return winCombos.some(checkWinCombo)
+}
+
+
+//
+
+//check if any of the remaining squares have a value of 0
 
 var isInArray = function (value, array) {
   return array.indexOf(value) > -1;
 }
 
-var emptySquares = function() {
-  if (isInArray(0, squares)) {
-    return true
-  } else {
-    gameActive = false
-    return false
-  }
+var emptySquaresLeft = function() {
+  return isInArray(0, squaresData)
 }
-
-//if game is still active, set whose turn it is, alternating from previous
-
-var changeTurn = function() {
-  if (gameActive = true) {
-    return playersTurn = !playersTurn
-  }
-}
-
-
-//GAME END
-
-//if gameActive is false and checkWinCombos is true, check if playerWins is true, and if so display win message. if false, display lose message.
-
-var winOrLose = function() {
-  if (!gameActive && checkWinCombos()) {
-    if (playerWins) {
-      alert("You Win! Grats!")
-    } else {
-      alert("You Lose. Try Again!")
-    }
-  }
-}
-
-
-//if emptySquares is false, and checkWinCombos is false, game is over with no winner. Display tie message.
-
-var tie = function() {
-  if (!emptySquares() && !checkWinCombos) {
-    alert("It's a tie! Try again.")
-  }
-}
-
-
-
-//reset game
-
-var refresh = //if user clicks refresh button, reset the array to all 0s, and reset setTurn to player's turn
-
 
 
 //EXECUTION
 
-promptPlayer()
+
+document.querySelector("table").addEventListener("click", function(e){
+
+  // player tries a move
+  var playerTakesSquare = assignSquare(squaresElems.indexOf(e.target), "player")
+
+  // if player picked occupied spot, return
+  if (!playerTakesSquare) return
+
+  // if someone won, the player won!
+  if (checkWinCombos()) {
+    return document.getElementById("message").innerHTML = "You Win! Grats!"
+  }
+
+  // if game is over, and noone won, it is a tie
+  if (!emptySquaresLeft()) {
+    return document.getElementById("message").innerHTML = "It's a tie! Try again."
+  }
+
+  // if game isnt over, let the AI go
+  assignSquare(aiSelect())
+
+  // if game is over, and someone won, the ai won!
+  if (checkWinCombos()) {
+    return document.getElementById("message").innerHTML = "You Lose. Try again!"
+  }
+
+  // if game is over, and noone won, it is a tie
+  if (!emptySquaresLeft()) {
+    return document.getElementById("message").innerHTML = "It's a tie! Try again."
+  }
+
+  // if game is not over, it is users turn again
+
+})
+
+//reset game - if user clicks refresh button, reset the array to all 0s and update the UI to match
+
+document.getElementById("refresh").addEventListener("click", function() {
+  squaresData = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  squaresElems.forEach(function(td){td.innerHTML = ""})
+  document.getElementById("message").innerHTML = ""
+})
