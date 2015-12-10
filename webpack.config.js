6bee4fa605bb588156e4a530f0995d106ad7df0c
Allison Keane Barr <allisonkeane@gmail.com>
2015-12-11 00:20:21 +0100

    moving bundle

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   devtool: 'eval',
   entry: [ './js/main.js' ].concat(extras),
   output: {
-    path: path.join(__dirname, 'js'),
+    path: __dirname,
     filename: 'bundle.js',
     publicPath: ''
   },
